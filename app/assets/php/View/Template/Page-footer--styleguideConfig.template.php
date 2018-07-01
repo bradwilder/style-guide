a74aef8ae37d5d479d06b1ccc19f487813764237
Brad <bradwilder31415@gmail.com>
2018-07-01 15:26:31 -0600

    Change App_styleguide_config to App_styleguideConfig

--- app/assets/php/View/Template/Page-footer--styleguideConfig.template.php
+++ app/assets/php/View/Template/Page-footer--styleguideConfig.template.php
@@ -1,5 +1,5 @@
-<!-- build:js ../../../../assets/scripts/App_styleguide_config.js -->
-<script src="../../../../temp/scripts/App_styleguide_config.js"></script>
+<!-- build:js ../../../../assets/scripts/App_styleguideConfig.js -->
+<script src="../../../../temp/scripts/App_styleguideConfig.js"></script>
 <!-- endbuild -->
 
 <?php include(__ASSETS_PATH . '/php/View/Template/Page-footer-end.template.php'); ?>
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports =
 		App_login: __dirname + "/app/assets/scripts/App_login.js",
 		App_moodboard: __dirname + "/app/assets/scripts/App_moodboard.js",
 		App_styleguide: __dirname + "/app/assets/scripts/App_styleguide.js",
-		App_styleguide_config: __dirname + "/app/assets/scripts/App_styleguide_config.js",
+		App_styleguideConfig: __dirname + "/app/assets/scripts/App_styleguideConfig.js",
 		App_admin: __dirname + "/app/assets/scripts/App_admin.js",
 		App_not_found: __dirname + "/app/assets/scripts/App_not_found.js",
 		Vendor: __dirname + "/app/assets/scripts/Vendor.js"
